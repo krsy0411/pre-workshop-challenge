59881b1cc4af5f43a8e0333d9a85fbada54f4fb6
PANdas <krsy0411@naver.com>
2024-07-12 16:32:54 +0900

    Timeout 시간 연장

--- workshop/AspireYouTubeSummariser.ServiceDefaults/Extensions.cs
+++ workshop/AspireYouTubeSummariser.ServiceDefaults/Extensions.cs
@@ -61,7 +61,7 @@ public static class Extensions
                 });
 
                 // See: https://www.pollydocs.org/strategies/timeout.html
-                builder.AddTimeout(TimeSpan.FromSeconds(120));
+                builder.AddTimeout(TimeSpan.FromSeconds(200));
             });
 
             // Turn on service discovery by default
